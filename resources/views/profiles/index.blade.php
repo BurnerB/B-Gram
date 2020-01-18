448146bf4c37d8094ba3731ab8a748922ccd53f8
BurnerB <barnabykamau@gmail.com>
2020-01-18 20:06:37 +0300

    update number of followers and following

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -24,8 +24,8 @@
             @endcan
             <div class="d-flex">
                 <div class="pr-4"><strong>{{ $user->posts->count() }}</strong> posts</div>
-                <div class="pr-4"><strong>1.1M</strong> followers</div>
-                <div class="pr-4"><strong>1</strong> follower</div>
+                <div class="pr-4"><strong>{{ $user->profile->followers->count()}}</strong> followers</div>
+                <div class="pr-4"><strong>{{ $user->following->count()}}</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
